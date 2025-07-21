225f318f8af25e52ce1078b462568f19d8ab2bb1
Anonymouskb17 <mancity452k3@gmail.com>
2025-07-21 12:21:48 +0700

    change

--- fe/src/main.jsx
+++ fe/src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import React from "react";
 import "./index.css";
-import Login from "./pages/Login";
+import Login from "./components/Login";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Signup from "./components/Signup";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Login />
   </StrictMode>
 );
